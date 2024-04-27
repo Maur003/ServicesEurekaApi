0ff372e7bf91be692e7db8769015300f9c748317
Mauro <AndyBoni_2012@hotmail.com>
2024-04-26 21:32:03 -0500

    SpringGateway & common microservices added

--- MicroservicioUsuarios/src/main/java/com/co/bonilla/usuarios/controller/AlumnoController.java
+++ MicroservicioUsuarios/src/main/java/com/co/bonilla/usuarios/controller/AlumnoController.java
@@ -3,11 +3,14 @@ package com.co.bonilla.usuarios.controller;
 import com.co.bonilla.usuarios.entity.Alumno;
 import com.co.bonilla.usuarios.service.AlumnoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -15,6 +18,19 @@ public class AlumnoController {
 
     @Autowired
     AlumnoService service;
+
+    @Value("${config.balanceador.test}")
+    private String balanceadorTest;
+
+    @GetMapping("/balanceador-Test")
+    public ResponseEntity<?> balanceadorTest(){
+        Map<String, Object> response = new HashMap<String, Object>();
+
+        response.put("balanceador", balanceadorTest);
+        response.put("alumno", service.findAll());
+
+        return ResponseEntity.ok().body(response);
+    }
 
     @GetMapping("/listarAlumnos")
     public ResponseEntity<?> listarAlumnos(){
